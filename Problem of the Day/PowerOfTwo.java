0d1e4fac42d36aa977ff68f191d98515fca684f6
Ethan Glover <glover.ethan@gmail.com>
2014-06-15 12:04:04 -0500

    Change PowOf2 Program
    Now utilizes logarithms.

--- Problem of the Day/PowerOfTwo.java
+++ Problem of the Day/PowerOfTwo.java
@@ -2,27 +2,23 @@
  * takes in a number and returns whether or not the number is a power of 2 or 
  * not.
  */
-import java.util.Scanner;
+import java.util.*;
 
 public class PowerOfTwo {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter number: ");
 		int n = input.nextInt();
-		checkPow(n);
+		System.out.println(checkPow(n));
 	}
 	
-	public static void checkPow(int n) {
-		int pow = 1;
-		for(int i = 0; i < n; i++) {
-			pow *= 2;
-			if(pow == n) {
-				System.out.println("true");
-				break;
-			} else if(pow > n) {
-				System.out.println("false");
-				break;
-			}
+	public static boolean checkPow(int n) {
+		double pow = Math.log(n) / Math.log(2);
+		System.out.println(pow);
+		if(pow == Math.floor(pow)) {
+			return true;
+		} else {
+			return false;
 		}
 	}
 }
