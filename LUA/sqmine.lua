276e4f278d9c82fc65550e7a935c4012b1cadf60
Ethan Glover <glover.ethan@gmail.com>
2014-06-21 18:59:19 -0500

    sqmine edits.
    Bug fixes for ComputerCraft square mine program.

--- LUA/sqmine.lua
+++ LUA/sqmine.lua
@@ -1,33 +1,92 @@
-count = 0 -- Global
+local count = 0 -- For torch function
 
-function torch()
+function torch() -- Place torch every 7 spots
 	turtle.select(1)
 	turtle.turnLeft()
-	turtle.dig()
+	gravelDig()
 	turtle.place()
 	turtle.turnRight()
+	while(turtle.getItemCount(1) == 0) do
+		turtle.dig()
+	end
 end
-function tunnelFor()
-	turtle.dig()
+
+function tunnelFor() -- Tunnel forward if inventory is good
+	gravelDig()
 	turtle.forward()
-	turtle.digUp()
+	gravelDigUp()
 	count = count + 1
 	if count % 7 == 0 then
 		torch()
 	end
-function tunnel(n)
+	while(turtle.getItemCount(16) > 0) do
+		turtle.dig()
+	end
+	checkWalls() -- Helps to avoid lava/water mishaps
 end
+
+function gravelDig() -- Dig forward, checking for gravel/sand
+	while(turtle.detect()) do
+		turtle.dig()
+	end
+end
+
+function gravelDigUp() -- Dig up, checking for gravel/sand
+	while(turtle.detectUp()) do
+		turtle.dig()
+	end
+end
+
+function checkWalls() -- Fill in walls to avoid water/lava problems
+	turtle.select(2)
+	while(turtle.getItemCount(2) < 6) do
+		turtle.dig()
+	end
+	if(turtle.detectDown() == false) then
+		turtle.placeDown()
+	end
+	turtle.turnLeft()
+	detectAndPlace()
+	turnAround()
+	detectAndPlace()
+	gravelDigUp()
+	turtle.up()
+	if(turtle.detectUp() == false) then
+		turtle.placeUp()
+	end
+	detectAndPlace()
+	turnAround()
+	detectAndPlace()
+	turtle.turnRight()
+	turtle.down();
+end
+
+function turnAround() -- 180 degree turn
+	turtle.turnRight()
+	turtle.turnRight()
+end
+
+function detectAndPlace() -- Place block if nothing there
+	if(turtle.detect() == false) then
+		turtle.place()
+	end
+end
+
+function tunnel(n) -- Dig a tunnel straight out
 	for i = 1,n-1 do
 		tunnelFor()
 	end
 	turtle.turnRight()
 end
-function tunnelLastWall(n)
+
+function tunnelLastWall(n) -- Dig fourth tunnel (shorter than normal tunnel)
 	for i = 1,n-4 do
 		tunnelFor()
 	end
 	turtle.turnRight()
-function driver(n)
+end
+
+function driver(n) -- Drivers whole program
 	for i = 1,3 do
 		tunnel(n)
 	end
@@ -38,7 +97,8 @@ function driver(n)
 end
 
 local n
-print("Enter size of square mine (less than 64): ");
+print("Torches in slot 1. Cobblestone in slot 2. Please and thank you!");
+print("Enter size of square mine (be reasonable!): ");
 n = tonumber(read())
 
 while n >= 0 do
